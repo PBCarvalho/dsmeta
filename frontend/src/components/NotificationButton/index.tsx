1a421fe56a03ab5e5e804f2e6f30fae3d5364917
PBCarvalho <peterson.carvalho20@gmail.com>
2022-07-15 20:17:02 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,6 @@
-import icon from '../../assets/img/Notification-icon.svg'
+import icon from '../../assets/img/Notification-icon.svg';
 
-import './styles.css'
+import './styles.css';
 
 function NotificationButton() {
     return (
@@ -10,4 +10,4 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
