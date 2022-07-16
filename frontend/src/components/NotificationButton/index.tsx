b2be260928f9cf51e035e1f5aba8fa8b53b7431c
PBCarvalho <peterson.carvalho20@gmail.com>
2022-07-16 16:59:19 -0300

    Update

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { toast } from "react-toastify";
-import icon from "../../assets/img/Notification-icon.svg";
+import icon from "../../assets/img/notification-icon.svg";
 import { BASE_URL } from "../../utils/request";
 
 import "./styles.css";
