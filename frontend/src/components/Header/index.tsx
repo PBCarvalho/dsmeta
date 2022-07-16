4d5d79eddd582f58ee136948249c368c586b75e1
PBCarvalho <peterson.carvalho20@gmail.com>
2022-07-16 18:10:29 -0300

    Update

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -10,7 +10,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    <a href="https://github.com/PBCarvalho">@PetersonCarvalho</a>
                 </p>
             </div>
         </header>
